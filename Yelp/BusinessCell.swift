5538f1f762a6ed783692ccf3e575a10855d5d004
Chappell M <Chappell@Chappells-MacBook-Pro.local>
2018-03-22 18:56:44 -0400

    Upload
    pwd

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -2,23 +2,51 @@
 //  BusinessCell.swift
 //  Yelp
 //
-//  Created by Chappell M on 3/21/18.
+//  Created by Collin S on 3/21/18.
 //  Copyright © 2018 Timothy Lee. All rights reserved.
 //
 
 import UIKit
 
 class BusinessCell: UITableViewCell {
-
+    
+    @IBOutlet weak var categoriesLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var reviewsCountLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var thumbView: UIImageView!
+    
+    var business: Business! {
+        didSet {
+            nameLabel.text = business.name
+            thumbView.setImageWith(business.imageURL!)
+            categoriesLabel.text = business.categories
+            addressLabel.text = business.address
+            reviewsCountLabel.text = "\(business.reviewCount!) Reviews"
+            ratingImageView.setImageWith(business.ratingImageURL!)
+            distanceLabel.text = business.distance
+        }
+    }
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        thumbView.layer.cornerRadius = 3
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
     }
-
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
